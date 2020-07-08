27ee7ca61ec7fd33c4e8344781a0f52d4062be1e
torecan <torecancelik@gmail.com>
2020-07-09 00:03:20 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -4,5 +4,31 @@ $( document ).ready(function() {
 
 // $(".bad_value").html("1");
 
-    
+
+  function httpGet("https://192.168.178.41:8123/lovelace/1/")
+{
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            console.log(xmlhttp.responseText);
+            return xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET", theUrl, false );
+    xmlhttp.send();    
+}
+
+
+
 });
+
+
