d084ed00f6a35d5e89e532e784c0d2a4bb69e036
torecan <torecancelik@gmail.com>
2020-07-09 00:07:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,28 +5,10 @@ $( document ).ready(function() {
 // $(".bad_value").html("1");
 
 
-  function httpGet("192.168.178.41:8123/lovelace/1/")
-{
-    if (window.XMLHttpRequest)
-    {// code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp=new XMLHttpRequest();
-    }
-    else
-    {// code for IE6, IE5
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange=function()
-    {
-        if (xmlhttp.readyState==4 && xmlhttp.status==200)
-        {
-            console.log(xmlhttp.responseText);
-            return xmlhttp.responseText;
-        }
-    }
-    xmlhttp.open("GET", theUrl, false );
-    xmlhttp.send();    
-}
 
+jQuery.get('192.168.178.41:8123/lovelace/1', function(data) {
+    console.log(data);
+});
 
 
 });
